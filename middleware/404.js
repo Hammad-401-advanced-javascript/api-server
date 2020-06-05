49be1660b417e4d14f1b4b01b20856d795e66e30
Hammadsoloman <hammadali95@yahoo.com>
2020-06-06 01:42:31 +0300

    finish the requirement for lab 10

--- middleware/404.js
+++ middleware/404.js
@@ -1,6 +1,19 @@
 'use strict';
 
-module.exports=(req,res)=>{
+/**
+ * (Middleware) set the response status to 404 and send an error
+ * @module 404
+ */
+
+/**
+ * Input 
+ * @param req - request
+ * @param res - response
+ * @param next - next
+ */
+
+
+module.exports=(req,res,next)=>{
   res.status(404);
   res.statusMessage='yourdata not found';
   res.json({error:'Not Found'});
--- middleware/500.js
+++ middleware/500.js
@@ -1,5 +1,20 @@
 'use strict';
 
+/**
+ * (Middleware) set the response status to 500 and send an error
+ * @module 500
+ */
+
+/**
+  * Input 
+  * @param req - request
+  * @param rse  - rseponse
+  * @param next
+  */
+
+
+
+
 module.exports = (err,req, res, next) => {
   res.status(500);
   res.statusMessage='Server Error';
